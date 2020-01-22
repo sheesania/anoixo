869c4bb30d532d9621be9ae01de07f1fb4e1a669
Alison Bell <sheesania@users.noreply.github.com>
2020-01-22 10:55:19 -0500

    connecting labels to text input fields

--- client/anoixo-client/src/query/Attributes/NLF/InflectedFormField.tsx
+++ client/anoixo-client/src/query/Attributes/NLF/InflectedFormField.tsx
@@ -1,10 +1,12 @@
 import React, {memo, ChangeEvent, useCallback} from 'react';
+import {useUID} from 'react-uid';
 import AttributeComponentProps from '../AttributeComponentProps';
 import AttributeEditor from '../AttributeEditor';
 import TextField from '@material-ui/core/TextField';
 import './InflectedFormField.css';
 
 const InflectedFormField: React.FC<AttributeComponentProps> = memo((props: AttributeComponentProps) => {
+  const uid = 'attr-field-' + useUID();
   const {updateAttr, id} = props;
   const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     updateAttr(id, event.target.value);
@@ -13,9 +15,9 @@ const InflectedFormField: React.FC<AttributeComponentProps> = memo((props: Attri
   const label = 'Inflected Form';
 
   return (
-    <AttributeEditor labelText={label}>
-        <TextField className='attribute-input inflected-form-selector' placeholder='Any' value={props.value || ''} 
-          onChange={onChange}/>
+    <AttributeEditor labelText={label} labelProps={{htmlFor: uid}}>
+        <TextField id={uid} className='attribute-input inflected-form-selector' placeholder='Any' 
+          value={props.value || ''} onChange={onChange}/>
     </AttributeEditor>
   );
 });
--- client/anoixo-client/src/query/Attributes/NLF/LexicalFormField.tsx
+++ client/anoixo-client/src/query/Attributes/NLF/LexicalFormField.tsx
@@ -1,10 +1,12 @@
 import React, {memo, ChangeEvent, useCallback} from 'react';
+import {useUID} from 'react-uid';
 import AttributeComponentProps from '../AttributeComponentProps';
 import AttributeEditor from '../AttributeEditor';
 import TextField from '@material-ui/core/TextField';
 import './LexicalFormField.css';
 
 const LexicalFormField: React.FC<AttributeComponentProps> = memo((props: AttributeComponentProps) => {
+  const uid = 'attr-field-' + useUID();
   const {updateAttr, id} = props;
   const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     updateAttr(id, event.target.value);
@@ -13,9 +15,9 @@ const LexicalFormField: React.FC<AttributeComponentProps> = memo((props: Attribu
   const label = 'Lexical Form';
 
   return (
-    <AttributeEditor labelText={label}>
-        <TextField className='attribute-input lexical-form-selector' placeholder='Any' value={props.value || ''} 
-          onChange={onChange}/>
+    <AttributeEditor labelText={label} labelProps={{htmlFor: uid}}>
+        <TextField id={uid} className='attribute-input lexical-form-selector' placeholder='Any' 
+          value={props.value || ''} onChange={onChange}/>
     </AttributeEditor>
   );
 });
