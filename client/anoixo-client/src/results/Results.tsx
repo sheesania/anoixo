e3b0f071a7251ac6177a45b61841a340846b3ff5
Alison Bell <sheesania@users.noreply.github.com>
2020-01-23 14:44:07 -0500

    Client makes requests to the server and has a path for errors

--- client/anoixo-client/src/results/Results.tsx
+++ client/anoixo-client/src/results/Results.tsx
@@ -5,6 +5,10 @@ import BackForwardButton from '../common/BackForwardButton';
 import Drawer from '@material-ui/core/Drawer';
 import './css/Results.css';
 
+// In the future, the server settings for a text could be grabbed dynamically from a dictionary depending on which text
+// the user selected. For now I am just hardcoding the NLF server.
+import {NLFServerSettings} from '../AppSettings';
+
 type Props = {
     query: Query;
     isOpen: boolean;
@@ -12,31 +16,57 @@ type Props = {
 }
 
 const Results: React.FC<Props> = (props: Props) => {
-    const {isOpen, closeSelf} = props;
+    const {query, isOpen, closeSelf} = props;
     const [results, setResults] = useState<string|undefined>(undefined);
+    const [error, setError] = useState<string|undefined>(undefined);
+
+    const handleRequestSuccess = (getJson: Promise<any>) => {
+        getJson.then((json: any) => {
+            setResults(JSON.stringify(json));
+        });
+    }
+    const handleRequestError = (getJson: Promise<any>) => {
+        getJson.then((json: any) => {
+            setError(JSON.stringify(json));
+        });
+    };
 
     useEffect(() => {
         if (!isOpen) {
             return;
         }
         setResults(undefined);
-        const promiseTimeout = (time: number) => new Promise(resolve => setTimeout(resolve, time));
-        promiseTimeout(1000).then(() => {
-            setResults('got results');
+        setError(undefined);
+        fetch(NLFServerSettings.textServerUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(query),
+        })
+        .then((response) => {
+            const getJson = response.json();
+            if (response.ok) {
+                handleRequestSuccess(getJson);
+            } else {
+                handleRequestError(getJson);
+            }
         });
-    }, [isOpen]);
+    }, [isOpen, query]);
+
+    let display;
+    if (error) {
+        display = <div>{error} <BackForwardButton type='back' onClick={closeSelf}/></div>;
+    } else if (results) {
+        display = <div>{results} <BackForwardButton type='back' onClick={closeSelf}/></div>;
+    } else {
+        display = <Loading closeSelf={closeSelf}/>;
+    }
 
     return (
         <Drawer open={isOpen} anchor='right'>
             <div className='results-content'>
-                {results ? (
-                    <div>
-                        {results}
-                        <BackForwardButton type='back' onClick={closeSelf}/>
-                    </div>
-                ) : (
-                    <Loading closeSelf={closeSelf}/>
-                )}
+                {display}
             </div>
         </Drawer>
     );
