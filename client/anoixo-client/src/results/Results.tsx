751fef28f0ff69329092d69d0779729afcf96829
Alison Bell <sheesania@users.noreply.github.com>
2020-01-23 12:45:05 -0500

    logic for only loading results when drawer is opened

--- client/anoixo-client/src/results/Results.tsx
+++ client/anoixo-client/src/results/Results.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Query} from '../query/QueryTypes';
 import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
@@ -10,10 +10,28 @@ type Props = {
 }
 
 const Results: React.FC<Props> = (props: Props) => {
+    const {isOpen, closeSelf} = props;
+    const [results, setResults] = useState<string|undefined>(undefined);
+
+    useEffect(() => {
+        if (!isOpen) {
+            return;
+        }
+        setResults(undefined);
+        const promiseTimeout = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+        promiseTimeout(1000).then(() => {
+            setResults('got results');
+        });
+    }, [isOpen]);
+
     return (
-        <Drawer className='Results' open={props.isOpen} anchor='right'>
-            results for query: {JSON.stringify(props.query)}
-            <Button onClick={props.closeSelf}>&lt;&lt;Back to Search</Button>
+        <Drawer className='Results' open={isOpen} anchor='right'>
+            {results ? (
+                <div>{results}</div>
+            ) : (
+                <div>Loading...</div>
+            )}
+            <Button onClick={closeSelf}>&lt;&lt;Back to Search</Button>
         </Drawer>
     );
 }
