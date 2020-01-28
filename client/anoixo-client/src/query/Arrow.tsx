aa9aa2a43e8d36da6b984248d201b44242c5221e
Alison Bell <sheesania@users.noreply.github.com>
2020-01-28 11:03:25 -0500

    arrows displayed between word cards with correct styling

--- client/anoixo-client/src/query/Arrow.tsx
+++ client/anoixo-client/src/query/Arrow.tsx
@@ -1,25 +1,27 @@
 import React, {memo} from 'react';
+import Typography from '@material-ui/core/Typography';
 
 type Props = {
     type: 'active' | 'inactive';
+    id: string;
 };
 
 const Arrow: React.FC<Props> = memo((props: Props) => {
     const color = props.type === 'active' ? 'black' : 'lightgray';
 
     return (
-        <svg width="98px" height="40px">
+        <svg id={props.id} width="98px" height="40px">
             <defs>
-                <marker id='arrowhead' markerWidth={28} markerHeight={40} refX={20} refY={20} orient='auto'
+                <marker id={`arrowhead${props.id}`} markerWidth={28} markerHeight={40} refX={20} refY={20} orient='auto'
                     markerUnits='userSpaceOnUse'>
                     <polyline points="8,8 20,20 8,32" stroke={color} strokeWidth="8" strokeLinecap="round" fill="none" 
                         stroke-linejoin="round"/>
                 </marker>
             </defs>
             <line x1={8} y1={20} x2={90} y2={20} stroke={color} strokeWidth={8} strokeLinecap="round" 
-                markerEnd='url(#arrowhead)'/>
+                markerEnd={`url(#arrowhead${props.id})`}/>
         </svg>
     );
-})
+});
 
 export default Arrow;
