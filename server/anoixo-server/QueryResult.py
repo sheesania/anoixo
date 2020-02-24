fecef72ffef38404b180078e440adb506100d15d
Alison Bell <sheesania@users.noreply.github.com>
2020-02-24 12:28:45 -0500

    Add translation to API result type

--- server/anoixo-server/QueryResult.py
+++ server/anoixo-server/QueryResult.py
@@ -65,12 +65,15 @@ class PassageResult:
             on_parsing_error('Result does not have a list of words')
         self.words = [WordResult(word, on_parsing_error) for word in json['words']]
 
+        self.translation = ''
+
     def __repr__(self):
         return f'{self.serialize()}'
 
     def serialize(self) -> Dict[str, List[Dict]]:
         return {'references': [reference.serialize() for reference in self.references],
-                'words': [word.serialize() for word in self.words]}
+                'words': [word.serialize() for word in self.words],
+                'translation': self.translation}
 
 
 class QueryResult:
