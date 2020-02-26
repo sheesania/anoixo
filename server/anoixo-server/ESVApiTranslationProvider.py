0930861b15e250b55eeebf90050d96b1f4af5d36
Alison Bell <sheesania@users.noreply.github.com>
2020-02-26 16:04:44 -0500

    Translations added to server response

--- server/anoixo-server/ESVApiTranslationProvider.py
+++ server/anoixo-server/ESVApiTranslationProvider.py
@@ -6,6 +6,19 @@ from QueryResult import QueryResult, Reference
 from TranslationProvider import TranslationProvider, TranslationProviderError
 
 
+class TranslationsForResultIndexes:
+    def __init__(self, response: Dict, result_start_index: int):
+        if not (isinstance(response, dict) and
+                'passages' in response and
+                isinstance(response['passages'], list)):
+            raise TranslationProviderError('Invalid response from ESV API')
+        self.translations: List[str] = response['passages']
+        self.result_start_index = result_start_index
+
+    def __repr__(self):
+        return f'Translations starting at index {self.result_start_index}: {{{self.translations}}}'
+
+
 class ESVApiTranslationProvider(TranslationProvider):
 
     """
@@ -33,20 +46,23 @@ class ESVApiTranslationProvider(TranslationProvider):
         else:
             return f'{references[0].string_ref}-{references[-1].string_ref}'
 
-    async def _send_query(self, session: aiohttp.ClientSession, chunk_verse_queries: List[str]) -> Dict:
+    async def _send_query(self, session: aiohttp.ClientSession, chunk_verse_queries: List[str],
+                          chunk_start_index: int) -> TranslationsForResultIndexes:
         query = ';'.join(chunk_verse_queries)
         print(query)
         resp = await session.get('https://api.esv.org/v3/passage/text',
                                  params={'q': query, **self.DEFAULT_REQUEST_PARAMS})
-        return await resp.json()
+        json = await resp.json()
+        return TranslationsForResultIndexes(json, chunk_start_index)
 
-    async def _request_translations(self, query_result: QueryResult) -> Dict:
+    async def _request_translations(self, query_result: QueryResult) -> List[TranslationsForResultIndexes]:
         async with aiohttp.ClientSession(
                 headers={'Authorization': Config.esv_api_key}) as session:
             result_index = 0
             verses_in_chunk_counter = 0
             query_string_length = self.STARTING_REQUEST_LINE_SIZE
             chunk_verse_queries: List[str] = []
+            chunk_start_index = 0
             requests = []
 
             while result_index < len(query_result.passages):
@@ -57,21 +73,31 @@ class ESVApiTranslationProvider(TranslationProvider):
 
                 # Partition requests into 300-verse chunks/4094-char request lines
                 if verses_in_chunk_counter > 300 or query_string_length > 4094:
-                    requests.append(self._send_query(session, chunk_verse_queries))
+                    requests.append(self._send_query(session, chunk_verse_queries, chunk_start_index))
                     verses_in_chunk_counter = 0
                     query_string_length = self.STARTING_REQUEST_LINE_SIZE
                     chunk_verse_queries = []
+                    chunk_start_index = result_index
                 else:
                     chunk_verse_queries.append(verse_query)
                     result_index += 1
 
             # if there is a remainder from the 300-verse/4094-char chunks
             if verses_in_chunk_counter > 0:
-                requests.append(self._send_query(session, chunk_verse_queries))
+                requests.append(self._send_query(session, chunk_verse_queries, chunk_start_index))
 
             results = await asyncio.gather(*requests)
             return results
 
+    def _add_translations_to_result(self, query_result: QueryResult,
+                                    translation_chunks: List[TranslationsForResultIndexes]) -> None:
+        for translation_chunk in translation_chunks:
+            result_start_index = translation_chunk.result_start_index
+            translations = translation_chunk.translations
+            for i in range(len(translations)):
+                query_result.passages[result_start_index + i].translation = translations[i]
+
     def add_translations(self, query_result: QueryResult) -> None:
         translations = asyncio.run(self._request_translations(query_result))
         print(translations)
+        self._add_translations_to_result(query_result, translations)
