33c74fb2657dab81eaf8fea953011b5262779ec6
Alison Bell <sheesania@users.noreply.github.com>
2020-02-27 20:17:52 -0500

    test for exception raising

--- server/anoixo-server/translation_providers/test_ESVApiTranslationProvider.py
+++ server/anoixo-server/translation_providers/test_ESVApiTranslationProvider.py
@@ -1,17 +1,25 @@
 import pytest
 from unittest.mock import AsyncMock
-from typing import Dict, List
+from typing import Callable, List
+from translation_providers.TranslationProvider import TranslationProviderError
 from translation_providers.ESVApiTranslationProvider import ESVApiTranslationProvider
 from QueryResult import QueryResult
 
 
-def mock_response(mocker, response: Dict):
+def mock_response(mocker, response_func: Callable):
     class MockResponse:
         async def json(self):
-            return response
+            return response_func()
     mock_get = mocker.patch('aiohttp.ClientSession.get', new_callable=AsyncMock)
     mock_get.return_value = MockResponse()
     return mock_get
+
+
+def make_mock_exception(mocker, exception_class_name):
+    class MockException(Exception):
+        pass
+    mock_exception = mocker.patch(exception_class_name, new_callable=lambda: MockException)
+    return mock_exception
 
 
 def query_result_for_json(json: List) -> QueryResult:
@@ -34,7 +42,7 @@ def test_get_translations_for_few_results(mocker, esv_provider: ESVApiTranslatio
             'words': []
         }
     ])
-    mock_response(mocker, {
+    mock_response(mocker, lambda: {
         'passages': [
             'text of Mark.1.1',
             'text of Matt.1.1-Matt.1.2',
@@ -47,8 +55,20 @@ def test_get_translations_for_few_results(mocker, esv_provider: ESVApiTranslatio
 
 def test_chunk_translation_api_requests(mocker, esv_provider: ESVApiTranslationProvider):
     result = query_result_for_json([{'references': ['John.1.1'], 'words': []} for _ in range(900)])
-    mock_get = mock_response(mocker, {
+    mock_get = mock_response(mocker, lambda: {
         'passages': ['text of John.1.1' for _ in range(300)]
     })
     esv_provider.add_translations(result)
     assert mock_get.call_count == 3
+
+
+def test_handles_content_type_error(mocker, esv_provider: ESVApiTranslationProvider):
+    mock_exception = make_mock_exception(mocker, 'aiohttp.ContentTypeError')
+
+    def raise_mock_exception():
+        raise mock_exception
+    mock_response(mocker, raise_mock_exception)
+
+    result = query_result_for_json([{'references': ['John.1.1'], 'words': []}])
+    with pytest.raises(TranslationProviderError):
+        esv_provider.add_translations(result)
