a5701ba4a42e0b2df4d49052244894d2595b57ba
Alison Bell <sheesania@users.noreply.github.com>
2020-02-28 10:43:34 -0500

    complete tests for PassageCard

--- client/anoixo-client/src/results/PassageCard.test.tsx
+++ client/anoixo-client/src/results/PassageCard.test.tsx
@@ -38,4 +38,83 @@ describe('PassageCard component', () => {
     expect(getByTextWithMarkup('ἐδάκρυσεν ὁ Ἰησοῦς.')).toBeInTheDocument();
     expect(getByText('Jesus wept.')).toBeInTheDocument();
   });
+
+  it('shows the reference correctly when it has multiple verses', () => {
+    const passageResult = {
+      references: [
+        {
+          book: 'John',
+          chapter: 11,
+          verse: 35,
+        },
+        {
+          book: 'John',
+          chapter: 11,
+          verse: 36,
+        },
+      ],
+      words: [],
+      translation: '',
+    };
+    const { getByText } = render(<PassageCard passage={passageResult} passageIndex={0} />);
+    expect(getByText('John 11:35-36')).toBeInTheDocument();
+  });
+
+  it('shows the reference correctly when it has multiple chapters', () => {
+    const passageResult = {
+      references: [
+        {
+          book: 'John',
+          chapter: 11,
+          verse: 35,
+        },
+        {
+          book: 'John',
+          chapter: 12,
+          verse: 1,
+        },
+      ],
+      words: [],
+      translation: '',
+    };
+    const { getByText } = render(<PassageCard passage={passageResult} passageIndex={0} />);
+    expect(getByText('John 11:35 - 12:1')).toBeInTheDocument();
+  });
+
+  it('highlights matched words', () => {
+    const passageResult = {
+      references: [
+        {
+          book: 'John',
+          chapter: 11,
+          verse: 35,
+        }
+      ],
+      words: [
+        {
+          text: 'ἐδάκρυσεν',
+          matchedSequence: 0,
+          matchedWordQuery: 0,
+        },
+        {
+          text: 'ὁ',
+          matchedSequence: 1,
+          matchedWordQuery: 0,
+        },
+        {
+          text: 'Ἰησοῦς.',
+          matchedSequence: -1,
+          matchedWordQuery: -1,
+        },
+      ],
+      translation: 'Jesus wept.',
+    };
+    const { getByText } = render(<PassageCard passage={passageResult} passageIndex={0} />);
+    const matchedFirstSequence = getByText('ἐδάκρυσεν');
+    expect(matchedFirstSequence.className).toBe('matched-word');
+    const matchedSecondSequence = getByText('ὁ');
+    expect(matchedSecondSequence.className).toBe('matched-word');
+    const didNotMatch = getByText('Ἰησοῦς.')
+    expect(didNotMatch.className).not.toBe('matched-word');
+  });
 });
