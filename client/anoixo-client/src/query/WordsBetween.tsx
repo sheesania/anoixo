027f803b53e16036e78a732f4d5a1170d1437d2a
Alison Bell <sheesania@users.noreply.github.com>
2020-03-04 16:38:49 -0500

    Validation before update

--- client/anoixo-client/src/query/WordsBetween.tsx
+++ client/anoixo-client/src/query/WordsBetween.tsx
@@ -32,37 +32,41 @@ const WordsBetween: React.FC<Props> = memo((props: Props) => {
   const { allowedWordsBetween, updateAllowedWordsBetween } = props;
   const uid = 'words-between-' + useUID();
   const [checked, setChecked] = useState((allowedWordsBetween || allowedWordsBetween === 0) ? true : false);
-  const [textFieldValue, setTextFieldValue] = useState(allowedWordsBetween || 0);
+  const [textFieldValue, setTextFieldValue] = useState((allowedWordsBetween || 0).toString());
+  const [error, setError] = useState(false);
   
+  // Validate and update allowedWordsBetween when the checkbox or text field is changed
   useEffect(() => {
+    const parsedTextFieldValue = parseInt(textFieldValue, 10);
+    if (Number.isNaN(parsedTextFieldValue)) {
+      setError(true);
+      if (checked) return;  // there is no valid new value, so don't update
+    } else {
+      setError(false);
+    }
+
     let newAllowedWordsBetween = undefined;
     if (checked) {
-      newAllowedWordsBetween = textFieldValue;
+      newAllowedWordsBetween = parsedTextFieldValue;
     }
     if (newAllowedWordsBetween !== allowedWordsBetween) {
       updateAllowedWordsBetween(newAllowedWordsBetween);
     }
-  }, [checked, textFieldValue, allowedWordsBetween, updateAllowedWordsBetween]);
-
-  const onCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-
-  const onTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTextFieldValue(parseInt(event.target.value, 10));
-  };
+  }, [checked, textFieldValue, setError, allowedWordsBetween, updateAllowedWordsBetween]);
 
   return (
     <div style={divStyle}>
-      <Checkbox id={uid} checked={checked} onChange={onCheckboxChange} />
+      <Checkbox id={uid} checked={checked} onChange={(event) => setChecked(event.target.checked)} />
       <Typography style={labelStyle} variant='subtitle1' component='label' htmlFor={uid}>
         up to 
         <TextField
           style={fieldDivStyle}
           value={textFieldValue}
+          error={error}
           variant='outlined'
           inputProps={{ style: inputStyle, size: 2 }}
-          onChange={onTextFieldChange}
+          onChange={(event) => setTextFieldValue(event.target.value)}
+          helperText={error ? 'Not a number' : undefined}
         />
         <br />
         words in between 
