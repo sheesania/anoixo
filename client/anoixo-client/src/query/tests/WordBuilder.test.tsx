c53855675aefb0d8613cb2a4c2192e2bee523e5d
Alison Bell <sheesania@users.noreply.github.com>
2020-03-13 22:12:11 -0400

    Logic for selecting an item in a select box

--- client/anoixo-client/src/query/tests/WordBuilder.test.tsx
+++ client/anoixo-client/src/query/tests/WordBuilder.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
+import getSelectItemFunction from '../../test/helpers/getSelectItem';
 import WordBuilder from '../WordBuilder';
 
 describe('WordBuilder component', () => {
@@ -46,6 +47,22 @@ describe('WordBuilder component', () => {
       const deleteButton = getByLabelText('Delete');
       fireEvent.click(deleteButton);
       expect(deleteWord).toHaveBeenCalledWith(0);
+    });
+  });
+
+  describe('attribute editors', () => {
+    it('disables irrelevant attributes when a part of speech is selected', async () => {
+      const { getByLabelText, getByText } = render(
+        <WordBuilder
+          word={{}}
+          wordIndex={0}
+          showDeleteButton={false}
+          updateWord={() => {}}
+          deleteWord={() => {}}
+        />
+      );
+      const selectItem = getSelectItemFunction(getByLabelText, getByText);
+      await selectItem('Part of Speech', 'Noun');
     });
   });
 });
