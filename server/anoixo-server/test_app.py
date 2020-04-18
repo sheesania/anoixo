bf5a761fd440510512c2b87c8662ca20a9025b46
Alison Bell <sheesania@users.noreply.github.com>
2020-04-18 19:08:44 -0400

    Tests for top-level app handling of errors

--- server/anoixo-server/test_app.py
+++ server/anoixo-server/test_app.py
@@ -1,9 +1,11 @@
 import pytest
 from app import app
 from text_providers.Nestle1904LowfatProvider import Nestle1904LowfatProvider
+from translation_providers.ESVApiTranslationProvider import ESVApiTranslationProvider
 from typing import Dict
 from werkzeug.wrappers import BaseResponse
 from AnoixoError import ServerOverwhelmedError
+from QueryResult import QueryResult
 
 
 @pytest.fixture
@@ -19,6 +21,106 @@ def client(mocker):
 def get_json_response(response: BaseResponse) -> Dict:
     import json
     return json.loads(response.get_data(as_text=True))
+
+
+def test_text_query_success(monkeypatch, client):
+    def mock_provider_text_query(self, text_query):
+        return QueryResult([{
+            'references': ['Mark.1.1'],
+            'words': [{
+                'matchedSequence': 0,
+                'matchedWordQuery': 0,
+                'text': 'word'
+            }]
+        }], lambda x: None)
+    monkeypatch.setattr(Nestle1904LowfatProvider, 'text_query', mock_provider_text_query)
+
+    def mock_add_translations(self, query_result):
+        for passage in query_result.passages:
+            passage.translation = 'translation text'
+    monkeypatch.setattr(ESVApiTranslationProvider, 'add_translations', mock_add_translations)
+
+    response = client.post('/api/text/nlf', json={'sequences': []})
+    assert response.status_code == 200
+    assert get_json_response(response) == [
+        {
+            'references': [{
+                'book': 'Mark',
+                'chapter': 1,
+                'verse': 1,
+            }],
+            'words': [{
+                'matchedSequence': 0,
+                'matchedWordQuery': 0,
+                'text': 'word'
+            }],
+            'translation': 'translation text'
+        }
+    ]
+
+
+def test_text_query_handles_no_json_given(client):
+    response = client.post('/api/text/nlf')
+    assert response.status_code == 400
+    assert get_json_response(response) == {
+        'error': 'Bad Request',
+        'description': 'Request does not contain a JSON body',
+        'friendlyErrorMessage': 'It looks like there\'s a bug in the app. Please let us know you had this problem so '
+                                'we can fix it!'
+    }
+
+
+def test_text_query_handles_invalid_json(client):
+    response = client.post('/api/text/nlf', json={'invalid': 'json'})
+    assert response.status_code == 400
+    assert get_json_response(response) == {
+        'error': 'Bad Request',
+        'description': 'Error parsing JSON: Does not contain a list \'sequences\'',
+        'friendlyErrorMessage': 'It looks like there\'s a bug in the app. Please let us know you had this problem so '
+                                'we can fix it!'
+    }
+
+
+def test_text_query_handles_text_not_found(client):
+    response = client.post('/api/text/invalid', json={'sequences': []})
+    assert response.status_code == 404
+    assert get_json_response(response) == {
+        'error': 'Not Found',
+        'description': 'Text provider with id \'invalid\' was not found. Available texts: nlf',
+        'friendlyErrorMessage': 'It looks like there\'s a bug in the app. Please let us know you had this problem so '
+                                'we can fix it!'
+    }
+
+
+def test_text_query_handles_text_provider_error(monkeypatch, client):
+    def mock_provider_text_query(self, query_result):
+        raise ServerOverwhelmedError('Error message')
+    monkeypatch.setattr(Nestle1904LowfatProvider, 'text_query', mock_provider_text_query)
+    response = client.post('/api/text/nlf', json={'sequences': []})
+    assert response.status_code == 500
+    assert get_json_response(response) == {
+        'error': 'Internal Server Error',
+        'description': 'Error message',
+        'friendlyErrorMessage': 'It looks like the server is currently overwhelmed. Try your search again later.'
+    }
+
+
+def test_text_query_handles_translation_provider_error(monkeypatch, client):
+    def mock_provider_text_query(self, query_result):
+        return QueryResult([{'references': ['Mark.1.1'], 'words': []}], lambda x: None)
+    monkeypatch.setattr(Nestle1904LowfatProvider, 'text_query', mock_provider_text_query)
+
+    def mock_add_translations(self, query_result):
+        raise ServerOverwhelmedError('Error message')
+    monkeypatch.setattr(ESVApiTranslationProvider, 'add_translations', mock_add_translations)
+
+    response = client.post('/api/text/nlf', json={'sequences': []})
+    assert response.status_code == 500
+    assert get_json_response(response) == {
+        'error': 'Internal Server Error',
+        'description': 'Error message',
+        'friendlyErrorMessage': 'It looks like the server is currently overwhelmed. Try your search again later.'
+    }
 
 
 def test_attribute_query_success(client):
