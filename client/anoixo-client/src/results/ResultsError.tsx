7960a634344e129e6f73ce863987a445cea51906
Alison Bell <sheesania@users.noreply.github.com>
2020-04-18 20:42:51 -0400

    Button for reporting problems

--- client/anoixo-client/src/results/ResultsError.tsx
+++ client/anoixo-client/src/results/ResultsError.tsx
@@ -3,6 +3,8 @@ import { ErrorResult } from './ResultTypes';
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import BackForwardButton from '../common/BackForwardButton';
+import Button from '@material-ui/core/Button';
+import { FEEDBACK_FORM_URL } from '../AppSettings';
 import './css/ResultsError.css';
 
 type Props = {
@@ -15,9 +17,20 @@ const ResultsError: React.FC<Props> = memo((props: Props) => {
 
   return (
     <div>
-      <Alert className='results-error-message' severity='error'>
+      <Alert className='results-error-alert' severity='error'>
         <AlertTitle>There was a problem doing your search</AlertTitle>
-        {error.friendlyErrorMessage}
+        <div className='results-error-message'>
+          {error.friendlyErrorMessage}
+        </div>
+        <Button
+          className='report-problem-button'
+          variant='outlined'
+          href={FEEDBACK_FORM_URL}
+          target='_blank'
+          rel='noopener'
+        >
+          Report your problem
+        </Button>
       </Alert>
       <BackForwardButton
         customStyling={{ marginLeft: '1.2rem', marginBottom: '1.2rem' }}
