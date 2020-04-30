17a8184882a06e1ae5395dc8df641be32b6e9db2
Alison Bell <sheesania@users.noreply.github.com>
2020-04-30 19:49:27 -0400

    remove unused import

--- client/anoixo-client/src/results/tests/VerbalizedQuery.test.tsx
+++ client/anoixo-client/src/results/tests/VerbalizedQuery.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { TextContextProvider } from '../../texts/TextContext';
 import { TextName } from '../../texts/TextSettings';
 import withMarkup from '../../test/helpers/withMarkup';
