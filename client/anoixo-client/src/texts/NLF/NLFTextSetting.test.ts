d891679c36904e00b6afa9dac6b6562b8c8b7cd9
Alison Bell <sheesania@users.noreply.github.com>
2020-04-30 13:10:12 -0400

    NLF verbalization function working and tested

---
+++ client/anoixo-client/src/texts/NLF/NLFTextSetting.test.ts
@@ -0,0 +1,118 @@
+import { NLFTextSetting } from './NLFTextSetting';
+
+describe('NLF verbalization', () => {
+  it('ignores incorrect attributes', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      wrong: 'attr'
+    });
+    expect(verbalization).toEqual('');
+  });
+
+  it('defaults to the attribute value if there is no defined display name', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      case: 'fake case'
+    });
+    expect(verbalization).toEqual('fake case');
+  });
+
+  it('orders descriptors properly', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      class: 'verbal',
+      case: 'nominative',
+      person: 'first',
+      number: 'singular',
+      gender: 'feminine',
+      tense: 'aorist',
+      voice: 'active',
+      mood: 'indicative',
+    });
+    expect(verbalization).toEqual('1st person singular nominative feminine aorist active indicative verbal');
+  });
+
+  it('uses participle as the part of speech if mood is set to participle, even if part of speech is also set', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      tense: 'future',
+      mood: 'participle',
+    });
+    expect(verbalization).toEqual('future participle');
+
+    const verbalizationWithReplace = NLFTextSetting.verbalizeAttributes({
+      class: 'verbal',
+      tense: 'future',
+      mood: 'participle',
+    });
+    expect(verbalizationWithReplace).toEqual('future participle');
+  });
+
+  it('uses infinitive as the part of speech if the mood is set to infinitive, even if part of speech ' +
+    'is also set', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      tense: 'future',
+      mood: 'infinitive',
+    });
+    expect(verbalization).toEqual('future infinitive');
+
+    const verbalizationWithReplace = NLFTextSetting.verbalizeAttributes({
+      class: 'verbal',
+      tense: 'future',
+      mood: 'infinitive',
+    });
+    expect(verbalizationWithReplace).toEqual('future infinitive');
+  });
+
+  it('handles lexical forms with no other attributes', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      lemma: 'ἀνοίγω'
+    });
+    expect(verbalization).toEqual('ἀνοίγω');
+  });
+
+  it('handles inflected forms with no other attributes', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      normalized: 'ἀνοίγω'
+    });
+    expect(verbalization).toEqual('ἀνοίγω');
+  });
+
+  it('if both inflected and lexical form are defined it goes with inflected form', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      lemma: 'ἀνοίγω',
+      normalized: 'ἀνοίξω',
+    });
+    expect(verbalization).toEqual('ἀνοίξω');
+  });
+
+  it('adds `from _` when there is a lexical form plus other descriptors', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      lemma: 'ἀνοίγω',
+      tense: 'future',
+      voice: 'passive',
+    });
+    expect(verbalization).toEqual('future passive from ἀνοίγω');
+  });
+
+  it('adds `from _` when there is an inflected form plus other descriptors', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      normalized: 'ἀνοιγήσεται',
+      tense: 'future',
+      voice: 'passive',
+    });
+    expect(verbalization).toEqual('future passive from ἀνοιγήσεται');
+  });
+
+  it('includes part of speech even if there is a lexical or inflected form', () => {
+    const verbalization = NLFTextSetting.verbalizeAttributes({
+      lemma: 'ἀνοίγω',
+      class: 'verbal',
+      tense: 'future',
+    });
+    expect(verbalization).toEqual('future verbal from ἀνοίγω');
+    const verbalizationWithReplace = NLFTextSetting.verbalizeAttributes({
+      lemma: 'ἀνοίγω',
+      class: 'verbal',
+      tense: 'future',
+      mood: 'participle',
+    });
+    expect(verbalizationWithReplace).toEqual('future participle from ἀνοίγω');
+  });
+});
