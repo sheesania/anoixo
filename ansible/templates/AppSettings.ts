cbe0619591c0025455904799aeb49b696ada733e
Alison Bell <sheesania@users.noreply.github.com>
2020-05-20 15:12:41 -0400

    Getting cert + https fully working

--- ansible/templates/AppSettings.ts
+++ ansible/templates/AppSettings.ts
@@ -1,5 +1,5 @@
 export const ServerSettings = {
-  apiUrl: '{{ "https" if https_enabled else "http" }}://{{ server_name }}/api',
+  apiUrl: '{{ "https" if enable_https else "http" }}://{{ server_name }}/api',
 };
 
 export const FEEDBACK_FORM_URL = "{{ feedback_form_url }}";
